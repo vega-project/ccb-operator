bf225b0f078f9c241849eaee57b8d1969c5ed19e
Nikolaos Moraitis <nmoraiti@redhat.com>
2021-11-29 22:47:42 +0100

    change the group name from the calculations custom resource
    Signed-off-by: Nikolaos Moraitis <nmoraiti@redhat.com>

--- pkg/apis/calculations/v1/doc.go
+++ pkg/apis/calculations/v1/doc.go
@@ -1,4 +1,4 @@
 // +k8s:deepcopy-gen=package,register
 
-// +groupName=vega.io
+// +groupName=vegaproject.io
 package v1
--- pkg/apis/calculations/v1/register.go
+++ pkg/apis/calculations/v1/register.go
@@ -16,7 +16,7 @@ func init() {
 }
 
 // SchemeGroupVersion is group version used to register these objects
-var SchemeGroupVersion = schema.GroupVersion{Group: "vega.io", Version: "v1"}
+var SchemeGroupVersion = schema.GroupVersion{Group: "vegaproject.io", Version: "v1"}
 
 // Kind takes an unqualified kind and returns back a Group qualified GroupKind
 func Kind(kind string) schema.GroupKind {
