337a2a6a263773931d624e2a6c59bd213fc46b12
Nikolaos Moraitis <nmoraiti@redhat.com>
2022-11-16 23:43:41 +0100

    initial implementation of the calculationbulkfactory api
    Signed-off-by: Nikolaos Moraitis <nmoraiti@redhat.com>

---
+++ pkg/apis/calculationbulkfactory/v1/types.go
@@ -0,0 +1,35 @@
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +kubebuilder:resource:shortName=cbf
+// +resource:path=calculationbulkfactory
+
+type CalculationBulkFactory struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	WorkerPool string   `json:"worker_pool,omitempty"`
+	BulkOutput string   `json:"bulk_output,omitempty"`
+	Command    string   `json:"command,omitempty"`
+	Args       []string `json:"args,omitempty"`
+
+	Status CalculationBulkFactoryStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +resource:path=calculationbulkfactories
+
+type CalculationBulkFactoryList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata"`
+	Items           []CalculationBulkFactory `json:"items"`
+}
+
+type CalculationBulkFactoryStatus struct {
+	CreatedTime    metav1.Time  `json:"startTime,omitempty"`
+	CompletionTime *metav1.Time `json:"completionTime,omitempty"`
+}
